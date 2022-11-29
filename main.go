789b44df0a9c9b8ffeeabb1436257085cf7dab3b
Darko Krizic <darko@krizic.net>
2022-11-29 17:49:01 +0100

    Now running gRPC and Proto

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/dkrizic/proto-demo/proto/todo"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net"
+	"net/http"
 )
 
 type server struct {
@@ -28,6 +31,31 @@ func main() {
 
 	s := grpc.NewServer()
 	todo.RegisterToDoServiceServer(s, NewServer())
-	log.Println("Listening on :8080")
-	log.Fatal(s.Serve(lis))
+	log.Println("Serving gRPC on :8080")
+	go func() {
+		log.Fatal(s.Serve(lis))
+	}()
+
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"localhost:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("Failed to dial", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	err = todo.RegisterToDoServiceHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatal("Failed to register gateway", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Service HTTP on :8090")
+	log.Fatal(gwServer.ListenAndServe())
 }
