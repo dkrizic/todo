f4c891cf4ed4d85f2f0af8715c018ba3c29a6ea3
Darko Krizic <darko@krizic.net>
2022-12-03 14:25:10 +0100

    Server implementation

--- server/notification/notification.go
+++ server/notification/notification.go
@@ -1,17 +1,28 @@
 package notification
 
-import "github.com/dapr/go-sdk/client"
+import (
+	"context"
+	"github.com/dapr/go-sdk/client"
+)
 
 type Notification struct {
-	Client client.Client
+	Client     client.Client
+	PubSubName string
+	TopicName  string
 }
 
-func NewNotification(client client.Client) *Notification {
-	return &Notification{
-		Client: client,
+func NewNotification(pubSubName string, topicName string) (notification *Notification, err error) {
+	daprClient, err := client.NewClient()
+	if err != nil {
+		return nil, err
 	}
+	return &Notification{
+		Client:     daprClient,
+		PubSubName: pubSubName,
+		TopicName:  topicName,
+	}, nil
 }
 
-func (n *Notification) SendNotification() error {
-	return n.Client.PublishEvent("pubsub", "notification", []byte("Hello World!"))
+func (n *Notification) SendNotification(message []byte) error {
+	return n.Client.PublishEvent(context.Background(), n.PubSubName, n.TopicName, message)
 }
