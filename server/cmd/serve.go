9b9968a2cb53726c9298b77a32a8f7e70f728adf
Darko Krizic <darko@krizic.net>
2023-01-17 16:54:31 +0100

    Initialize opentracing

--- server/cmd/serve.go
+++ server/cmd/serve.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"github.com/dkrizic/todo/server/sender"
+	opentracing "github.com/opentracing/opentracing-go"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"io"
 )
 
 const (
@@ -14,9 +18,13 @@ const (
 	notificationsEnabledFlag     = "sender-enabled"
 	notificationsPubSubNameFlag  = "sender-pubsub-name"
 	notificationsPubSubTopicFlag = "sender-pubsub-topic"
+	enableTracingFlag            = "enable-tracing"
+	tracingUrlFlag               = "tracing-url"
 )
 
 var senderClient *sender.Sender
+var tracer opentracing.Tracer
+var closer io.Closer
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
@@ -25,6 +33,39 @@ var serveCmd = &cobra.Command{
 	Long: `There are multiple backends available for the service like
 memory, redis, etc. This command will start the service with the
 given backend.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		tracingEnabled := viper.GetBool(enableTracingFlag)
+		tracingUrl := viper.GetString(tracingUrlFlag)
+		log.WithFields(log.Fields{
+			"tracingEnabled": tracingEnabled,
+			"tracingUrl":     tracingUrl,
+		}).Info("Tracing configuration")
+		if tracingEnabled {
+			cfg := jaegercfg.Configuration{
+				ServiceName: "todo",
+				Sampler: &jaegercfg.SamplerConfig{
+					Type:  "const",
+					Param: 1,
+				},
+				Reporter: &jaegercfg.ReporterConfig{
+					LogSpans:           true,
+					LocalAgentHostPort: tracingUrl,
+				},
+			}
+
+			var err error
+			tracer, closer, err = cfg.NewTracer()
+			if err != nil {
+				log.Fatalf("Could not initialize jaeger tracer: %s", err.Error())
+			}
+			opentracing.SetGlobalTracer(tracer)
+		} else {
+			log.Info("Tracing disabled")
+		}
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		closer.Close()
+	},
 	ValidArgs: []string{"memory", "redis"},
 }
 
@@ -38,6 +79,8 @@ func init() {
 	serveCmd.PersistentFlags().BoolP(notificationsEnabledFlag, "n", false, "Enable notifications")
 	serveCmd.PersistentFlags().StringP(notificationsPubSubNameFlag, "", "todo-pubsub", "The name of the pubsub component to use for notifications")
 	serveCmd.PersistentFlags().StringP(notificationsPubSubTopicFlag, "", "todo", "The name of the topic to use for notifications")
+	serveCmd.PersistentFlags().BoolP(enableTracingFlag, "t", false, "Enable tracing")
+	serveCmd.PersistentFlags().StringP(tracingUrlFlag, "", "http://localhost:14268/api/traces", "The url of the tracing server")
 	viper.BindEnv(httpPortFlag, "TODO_HTTP_PORT")
 	viper.BindEnv(grpcPortFlag, "TODO_GRPC_PORT")
 	viper.BindEnv(healthPortFlag, "TODO_HEALTH_PORT")
@@ -45,4 +88,6 @@ func init() {
 	viper.BindEnv(notificationsEnabledFlag, "TODO_NOTIFICATIONS_ENABLED")
 	viper.BindEnv(notificationsPubSubNameFlag, "TODO_NOTIFICATIONS_PUBSUB_NAME")
 	viper.BindEnv(notificationsPubSubTopicFlag, "TODO_NOTIFICATIONS_PUBSUB_TOPIC")
+	viper.BindEnv(enableTracingFlag, "TODO_ENABLE_TRACING")
+	viper.BindEnv(tracingUrlFlag, "TODO_TRACING_URL")
 }
--- server/go.mod
+++ server/go.mod
@@ -23,6 +23,8 @@ require (
 	github.com/cloudevents/sdk-go/v2 v2.13.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -33,6 +35,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -43,6 +46,11 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/uber/jaeger-client-go v2.30.0+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	go.opentelemetry.io/otel v1.11.2 // indirect
+	go.opentelemetry.io/otel/sdk v1.11.2 // indirect
+	go.opentelemetry.io/otel/trace v1.11.2 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
