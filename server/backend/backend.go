4d84852f66ed1b8125144d23d31cc607a24ca959
Darko Krizic <darko@krizic.net>
2023-01-24 16:57:32 +0100

    Actually writing data

--- server/backend/backend.go
+++ server/backend/backend.go
@@ -100,7 +100,7 @@ func extracaDataFromRequest(ctx context.Context, r *http.Request) (data []byte, 
 	return data, nil
 }
 
-func convertTodoStructToJson(ctx context.Context, todo repository.Todo) (jsonData []byte, err error) {
+func convertTodoStructToJson(ctx context.Context, todo []*repository.Todo) (jsonData []byte, err error) {
 	_, span := otel.Tracer("backend").Start(ctx, "convertTodoStructToJson")
 	defer span.End()
 	jsonData, err = json.Marshal(todo)
@@ -120,12 +120,18 @@ func TodosHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		err = json.NewEncoder(w).Encode(response.Todos)
+		data, err := convertTodoStructToJson(ctx, response.Todos)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
 	case "POST":
 		data, err := extracaDataFromRequest(ctx, r)
 		if err != nil {
