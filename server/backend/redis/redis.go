eafafd413a66b1d855dddbaa3cebf861b7c20c36
Darko Krizic <darko@krizic.net>
2023-01-27 12:21:53 +0100

    Activate redis read code again

--- server/backend/redis/redis.go
+++ server/backend/redis/redis.go
@@ -6,6 +6,7 @@ import (
 	redis "github.com/go-redis/redis/v9"
 	log "github.com/sirupsen/logrus"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"strconv"
 )
 
@@ -101,51 +102,47 @@ func (s *server) GetAll(ctx context.Context, req *repository.GetAllRequest) (res
 	defer span.End()
 	log.Info("Getting all todos")
 
-	//var cursor uint64 = 0
-	//todos := make([]*repository.Todo, 0)
-	//for {
-	//	var keys []string
-	//	var err error
-	//	ctx2, span2 := otel.Tracer("redis").Start(ctx, "Scan")
-	//	keys, cursor, err = s.RedisAdapter.redis.Scan(ctx2, cursor, "", 10).Result()
-	//	span2.End()
-	//	if err != nil {
-	//		log.WithError(err).Fatal("Failed to get keys")
-	//		return nil, err
-	//	}
-	//	for _, key := range keys {
-	//		log.WithField("key", key).Info("Found key")
-	//		var title string
-	//		var description string
-	//		{
-	//			ctx2, span := otel.Tracer("redis").Start(ctx, "GetAll/ReadTitle")
-	//			span.SetAttributes(attribute.String("id", key))
-	//			title = s.RedisAdapter.redis.HGet(ctx2, key, title).Val()
-	//			span.End()
-	//		}
-	//		{
-	//			ctx2, span := otel.Tracer("redis").Start(ctx, "GetAll/ReadDescription")
-	//			span.SetAttributes(attribute.String("id", key))
-	//			description = s.RedisAdapter.redis.HGet(ctx2, key, description).Val()
-	//			span.End()
-	//		}
-	//		todos = append(todos, &repository.Todo{
-	//			Id:          key,
-	//			Title:       title,
-	//			Description: description,
-	//		})
-	//	}
-	//	if cursor == 0 {
-	//		break
-	//	}
-	//}
-
-	//return &repository.GetAllResponse{
-	//	Todos: todos,
-	//}, nil
+	var cursor uint64 = 0
+	todos := make([]*repository.Todo, 0)
+	for {
+		var keys []string
+		var err error
+		ctx2, span2 := otel.Tracer("redis").Start(ctx, "Scan")
+		keys, cursor, err = s.RedisAdapter.redis.Scan(ctx2, cursor, "", 10).Result()
+		span2.End()
+		if err != nil {
+			log.WithError(err).Fatal("Failed to get keys")
+			return nil, err
+		}
+		for _, key := range keys {
+			log.WithField("key", key).Info("Found key")
+			var title string
+			var description string
+			{
+				ctx2, span := otel.Tracer("redis").Start(ctx, "GetAll/ReadTitle")
+				span.SetAttributes(attribute.String("id", key))
+				title = s.RedisAdapter.redis.HGet(ctx2, key, title).Val()
+				span.End()
+			}
+			{
+				ctx2, span := otel.Tracer("redis").Start(ctx, "GetAll/ReadDescription")
+				span.SetAttributes(attribute.String("id", key))
+				description = s.RedisAdapter.redis.HGet(ctx2, key, description).Val()
+				span.End()
+			}
+			todos = append(todos, &repository.Todo{
+				Id:          key,
+				Title:       title,
+				Description: description,
+			})
+		}
+		if cursor == 0 {
+			break
+		}
+	}
 
 	return &repository.GetAllResponse{
-		Todos: []*repository.Todo{},
+		Todos: todos,
 	}, nil
 }
 
