a97b4e8b91d7481d25e2a53b9b911f24af22ba4e
Darko Krizic <darko@krizic.net>
2023-01-29 18:57:59 +0100

    Pass traceID explicitly

--- server/sender/sender.go
+++ server/sender/sender.go
@@ -5,35 +5,15 @@ import (
 	dapr "github.com/dapr/go-sdk/client"
 	log "github.com/sirupsen/logrus"
 	"go.opentelemetry.io/otel"
-	"time"
 )
 
 type Sender struct {
-	client     dapr.Client
 	PubSubName string
 	TopicName  string
 }
 
 func NewSender(pubSubName string, topicName string) (notification *Sender, err error) {
-	var daprClient dapr.Client
-	// try 10 times to connect to dapr and wait 1 seconds after each try
-	max := 10
-	for i := 0; i < max; i++ {
-		daprClient, err = dapr.NewClient()
-		if err == nil {
-			break
-		}
-		log.WithFields(log.Fields{
-			"try": i,
-			"max": max,
-		}).WithError(err).Warn("Unable to connect to dapr")
-		time.Sleep(1 * time.Second)
-	}
-	if err != nil {
-		return nil, err
-	}
 	return &Sender{
-		client:     daprClient,
 		PubSubName: pubSubName,
 		TopicName:  topicName,
 	}, nil
@@ -49,7 +29,14 @@ func (n *Sender) SendNotification(ctx context.Context, message []byte) error {
 		"message":    string(message),
 	})
 	llog.Debug("Sending sender")
-	err := n.client.PublishEvent(ctx, n.PubSubName, n.TopicName, message)
+	client, err := dapr.NewClient()
+	if err != nil {
+		log.WithError(err).Warn("Unable to create dapr client")
+		span.RecordError(err)
+		return err
+	}
+	client.WithTraceID(ctx, span.SpanContext().TraceID().String())
+	err = client.PublishEvent(ctx, n.PubSubName, n.TopicName, message)
 	if err != nil {
 		llog.WithError(err).Warn("Unable to send sender")
 		return err
